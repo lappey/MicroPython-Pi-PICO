080338d5fb9c5729bdb0346d697406f4695973d9
lappey <65673113+lappey@users.noreply.github.com>
2026-01-02 15:28:07 +0000

    Delete Blink.py

--- Blink.py
+++
@@ -1,9 +0,0 @@
-import machine
-import time
-led_external = machine.Pin(15, machine.Pin.OUT)
-led_onboard = machine.Pin("LED", machine.Pin.OUT)
-
-while True:
-    led_onboard.toggle()
-    time.sleep(2)
-    led_external.toggle()
