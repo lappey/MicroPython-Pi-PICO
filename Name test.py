61c490eebc0282ed24ee3f9410598f4a8faae8cf
lappey <65673113+lappey@users.noreply.github.com>
2026-01-02 15:28:25 +0000

    Delete Name test.py

--- Name test.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user_name = input("What is your name? ")
-    if user_name == "Clark Kent":
-        print("You are Superman")
-        break
-    else:
-        print("You are not Superman")
